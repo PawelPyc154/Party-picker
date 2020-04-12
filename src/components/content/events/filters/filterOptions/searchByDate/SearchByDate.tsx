47f18ef5046a52be2b2cc57dbac71b8ce1bb9cbe
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-04-12 15:38:15 +0200

    add search by data

--- src/components/content/events/filters/filterOptions/searchByDate/SearchByDate.tsx
+++ src/components/content/events/filters/filterOptions/searchByDate/SearchByDate.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React from 'react';
+import Slider from '@material-ui/core/Slider';
 
 export interface SearchByDateProps {}
 
 const SearchByDate: React.SFC<SearchByDateProps> = () => {
-  return <div>y</div>;
+  const [value, setValue] = React.useState([20, 37]);
+  const handleChange = (event: any, newValue: any) => {
+    setValue(newValue);
+  };
+
+  function valuetext(value: any) {
+    return `${value}°C`;
+  }
+
+  return (
+    <Slider
+      value={value}
+      onChange={handleChange}
+      valueLabelDisplay="auto"
+      aria-labelledby="range-slider"
+      getAriaValueText={valuetext}
+    />
+  );
 };
 
 export default SearchByDate;
