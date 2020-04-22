53e586ed7b0e429402da9c0630f2c778cd713c9b
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-04-22 14:53:48 +0200

    add reducer add point event

--- src/state/allReducers.ts
+++ src/state/allReducers.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import EventsReducer from './events/reducer';
 import AuthReducer from './auth/reducer';
+import PositionAddEventReducer from './positionAddEvent/reducer';
 
 const allReducers = combineReducers({
   EventsReducer,
   AuthReducer,
+  PositionAddEventReducer,
 });
 
 export type AppState = ReturnType<typeof allReducers>;
