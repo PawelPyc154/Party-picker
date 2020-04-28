7f997b78459a2f699a3440c5b3c032d84eb707ce
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-04-28 18:59:58 +0200

    delete locaStorade filters

--- src/context/GetAndFilterEvents.tsx
+++ src/context/GetAndFilterEvents.tsx
@@ -31,19 +31,15 @@ const GetAndFilterEvent: React.FC<GetAndFilterEventProps> = ({ children }) => {
   const [filters, setFilters] = useState<Filters>({
     name: localStorage.getItem('filters-name') || '',
     province: localStorage.getItem('filters-province') || '',
-    timeFromTo: JSON.parse(
-      localStorage.getItem('filters-timeFromTo') || JSON.stringify(initialTimeFromTo),
-    ),
+    timeFromTo: JSON.parse(localStorage.getItem('filters-timeFromTo') || JSON.stringify(initialTimeFromTo)),
   });
 
   useEffect(() => {
     setEventsFiltered(
       events
         .filter((item) => {
-          const timeFrom =
-            Date.now() - 1000 * 60 * 60 * 24 * 8 + filters.timeFromTo[0] * ((1000 * 60 * 60) / 2);
-          const timeTo =
-            Date.now() - 1000 * 60 * 60 * 24 * 8 + filters.timeFromTo[1] * ((1000 * 60 * 60) / 2);
+          const timeFrom = Date.now() - 1000 * 60 * 60 * 24 * 8 + filters.timeFromTo[0] * ((1000 * 60 * 60) / 2);
+          const timeTo = Date.now() - 1000 * 60 * 60 * 24 * 8 + filters.timeFromTo[1] * ((1000 * 60 * 60) / 2);
           return (
             item.name.includes(filters.name) &&
             item.province.includes(filters.province) &&
@@ -57,9 +53,7 @@ const GetAndFilterEvent: React.FC<GetAndFilterEventProps> = ({ children }) => {
 
   const handleChangeFilters = (value: string, filterProperty: keyof Filters) => {
     setFilters((prev: Filters) => {
-      if (prev[filterProperty] === value) {
-        return { ...prev, [filterProperty]: '' };
-      }
+      if (prev[filterProperty] === value) return { ...prev, [filterProperty]: '' };
       return { ...prev, [filterProperty]: value };
     });
   };
@@ -70,22 +64,20 @@ const GetAndFilterEvent: React.FC<GetAndFilterEventProps> = ({ children }) => {
 
   // localStorage;
   useEffect(() => {
-    if (filters.name !== '') {
-      localStorage.setItem('filters-name', filters.name);
-    }
+    if (filters.name === '') return localStorage.removeItem('filters-name');
+    return localStorage.setItem('filters-name', filters.name);
   }, [filters.name]);
+
   useEffect(() => {
-    if (filters.province !== '') {
-      localStorage.setItem('filters-province', filters.province);
-    }
+    if (filters.province === '') return localStorage.removeItem('filters-province');
+    return localStorage.setItem('filters-province', filters.province);
   }, [filters.province]);
+
   useEffect(() => {
-    if (
-      filters.timeFromTo[0] !== initialTimeFromTo[0] ||
-      filters.timeFromTo[1] !== initialTimeFromTo[1]
-    ) {
-      localStorage.setItem('filters-timeFromTo', JSON.stringify(filters.timeFromTo));
+    if (filters.timeFromTo[0] !== initialTimeFromTo[0] || filters.timeFromTo[1] !== initialTimeFromTo[1]) {
+      return localStorage.setItem('filters-timeFromTo', JSON.stringify(filters.timeFromTo));
     }
+    return localStorage.removeItem('filters-timeFromTo');
   }, [filters.timeFromTo, initialTimeFromTo]);
 
   return (
