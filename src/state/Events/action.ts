a5bf809272f1998290a860df924f8279b37501cb
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-08-06 12:23:57 +0200

    delete trycatch

--- src/state/Events/action.ts
+++ src/state/Events/action.ts
@@ -3,10 +3,6 @@ import { EventsActionTypes, GET_EVENTS } from './actionType';
 import axiosWithConfig from '../../utils/axiosWithConfig';
 
 export const getEvents = () => async (dispatch: ThunkDispatch<{}, {}, EventsActionTypes>) => {
-  try {
-    const res = await axiosWithConfig.get('/events');
-    dispatch({ type: GET_EVENTS, payload: res.data.data });
-  } catch (err) {
-    console.log('bad request');
-  }
+  const res = await axiosWithConfig.get('/events');
+  dispatch({ type: GET_EVENTS, payload: res.data.data });
 };
