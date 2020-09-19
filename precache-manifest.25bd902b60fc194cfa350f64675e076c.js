783897be20522b798383009bf3d6692fc8262207
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-09-19 20:56:57 +0200

    Updates

---
+++ precache-manifest.25bd902b60fc194cfa350f64675e076c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398db62c61deb5cc2e800ae7a0f3e5c9",
+    "url": "/Party-picker/index.html"
+  },
+  {
+    "revision": "1154696858f552fb6eee",
+    "url": "/Party-picker/static/css/1.8125915a.chunk.css"
+  },
+  {
+    "revision": "e9e15a7256fdbf54c2d7",
+    "url": "/Party-picker/static/js/0.a8fad847.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/Party-picker/static/js/0.a8fad847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1154696858f552fb6eee",
+    "url": "/Party-picker/static/js/1.2d35a979.chunk.js"
+  },
+  {
+    "revision": "6667dc153edcbe95e8723d6c8f6541e2",
+    "url": "/Party-picker/static/js/1.2d35a979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e0f1e8c98502893b7f",
+    "url": "/Party-picker/static/js/10.364f18b3.chunk.js"
+  },
+  {
+    "revision": "f07cc6ea49a9b975d5c8",
+    "url": "/Party-picker/static/js/11.81f8eaa2.chunk.js"
+  },
+  {
+    "revision": "c794919c0c16698dcd06",
+    "url": "/Party-picker/static/js/4.f1935351.chunk.js"
+  },
+  {
+    "revision": "a73d84932f5c5059bb8bd8e560ce955f",
+    "url": "/Party-picker/static/js/4.f1935351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab3d6d135d695148af60",
+    "url": "/Party-picker/static/js/5.eb359fe5.chunk.js"
+  },
+  {
+    "revision": "0cc075dd87f75a3788db",
+    "url": "/Party-picker/static/js/6.a07d6a0a.chunk.js"
+  },
+  {
+    "revision": "894b8616d937f53efef8",
+    "url": "/Party-picker/static/js/7.682b4eb3.chunk.js"
+  },
+  {
+    "revision": "26e26cc8bff0c65827f1",
+    "url": "/Party-picker/static/js/8.458ef720.chunk.js"
+  },
+  {
+    "revision": "e0390063e92f590a961b",
+    "url": "/Party-picker/static/js/9.f9782263.chunk.js"
+  },
+  {
+    "revision": "21a2829f4f5abb1dc4aa",
+    "url": "/Party-picker/static/js/main.f37a610b.chunk.js"
+  },
+  {
+    "revision": "9aaf33a624c60ea6ba0a",
+    "url": "/Party-picker/static/js/runtime-main.6f9d8932.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Party-picker/precache-manifest.12e4b2b9cd144ef058e9a64b8861cbd0.js"
+  "/Party-picker/precache-manifest.25bd902b60fc194cfa350f64675e076c.js"
 );
 
 self.addEventListener('message', (event) => {
